e6c393fb3ac6eace61fea8613456573ed40d62e2
wannabedigital <digitaldxmmy@gmail.com>
2025-12-09 01:37:38 +0300

    feat(auth): store user data in local storage after log in, sign in

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,35 +1,42 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  isAuth: false,
+export const useAuthStore = create(
+  persist((set) => ({
+    user: null,
+    isAuth: false,
 
-  register: (userData) =>
-    set(
-      {
-        user: userData,
-        isAuth: true,
-      },
-      console.log('Вы зарегестрированы'),
-      console.log(userData)
-    ),
+    register: (userData) =>
+      set(
+        {
+          user: userData,
+          isAuth: true,
+        },
+        console.log('Вы зарегестрированы'),
+        console.log(userData)
+      ),
 
-  login: (userData) =>
-    set(
-      {
-        user: userData,
-        isAuth: true,
-      },
-      console.log('Вы вогли в аккаунт'),
-      console.log(userData)
-    ),
+    login: (userData) =>
+      set(
+        {
+          user: userData,
+          isAuth: true,
+        },
+        console.log('Вы вогли в аккаунт'),
+        console.log(userData)
+      ),
 
-  logout: () =>
-    set(
-      {
-        user: null,
-        isAuth: false,
-      },
-      console.log('Вы вышли из аккаунта')
-    ),
-}));
+    logout: () =>
+      set(
+        {
+          user: null,
+          isAuth: false,
+        },
+        console.log('Вы вышли из аккаунта')
+      ),
+  })),
+  {
+    name: 'auth-storage',
+    storage: createJSONStorage(() => localStorage),
+  }
+);
